7ab9e568e9f2c76d0782fe5ad184ac1163049561
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-05-25 17:07:57 +0200

    Implemented first iteration of Board.turnP1()

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Board {
 
     private final char player1 = 'x';
@@ -5,6 +7,8 @@ public class Board {
     private final char empty = ' ';
 
     private Field[][] fields;
+
+    Scanner sc = new Scanner(System.in);
 
     public Board() {
         fields = new Field[6][7];
@@ -15,41 +19,62 @@ public class Board {
         }
     }
 
-    public void run() {
-        if(Math.random() < 0.5) {
-            System.out.println("The coin flipped heads! Player 1 (x, red) will begin!");
-            //draw()
-            while(true) {
-                //turnP1()
-                //draw()
-                //winCheck()
-                //turnP2()
-                //draw()
-                //winCheck()
-            }
-        } else {
-            System.out.println("The coin flipped tails! Player 2 (o, yellow) will begin!");
-            //draw()
-            while(true) {
-                //turnP2()
-                //draw()
-                //winCheck()
-                //turnP1()
-                //draw()
-                //winCheck()
-            }
-        }
-    }
-
     public void draw() {
         System.out.println();
-        System.out.println("   A B C D E F G");
+        System.out.println(" 1 2 3 4 5 6 7");
         for(int y = 0; y < fields.length; y++) {
-            System.out.print((y + 1) + " |");
+            System.out.print("|");
             for(int x = 0; x < fields[0].length; x++) {
                 System.out.print(fields[y][x].getStatus() + "|");
             }
             System.out.println();
         }
+    }
+
+    public void turnP1() {
+        char turn;
+        System.out.println("Player 1 (x, red), enter column for your turn (1, 2, 3, 4, 5, 6, 7) and then press enter!");
+        while(!input(turn = sc.next().charAt(0))) {
+            if((turn == '1') || (turn == '2') || (turn == '3') || (turn == '4') || (turn == '5') || (turn == '6') || (turn == '7')) {
+                int x = Character.getNumericValue(turn);
+                for(int y = 0; y < fields.length; y++) {
+                    if(!fields[y][x].isOccupied()) {
+                        if(!fields[y + 1][x].isOccupied()) {
+                            fields[y][x].setStatus(player1);
+                            fields[y][x].setOccupied(true);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public boolean input(char turn) {
+        if((turn == '1') || (turn == '2') || (turn == '3') || (turn == '4') || (turn == '5') || (turn == '6') || (turn == '7')) {
+            return true;
+        } else {
+            System.out.println("Error, invalid input!");
+            return false;
+        }
+    }
+    public char getPlayer1() {
+        return player1;
+    }
+
+    public char getPlayer2() {
+        return player2;
+    }
+
+    public char getEmpty() {
+        return empty;
+    }
+
+    public Field[][] getFields() {
+        return fields;
+    }
+
+    public void setFields(Field[][] fields) {
+        this.fields = fields;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,30 @@ public class Main {
         Board board = new Board();
         board.draw();
     }
+
+    public void run() {
+        if (Math.random() < 0.5) {
+            System.out.println("The coin flipped heads! Player 1 (x, red) will begin!");
+            //board.draw()
+            while (true) {
+                //board.turnP1()
+                //board.draw()
+                //board.winCheck()
+                //board.turnP2()
+                //board.draw()
+                //board.winCheck()
+            }
+        } else {
+            System.out.println("The coin flipped tails! Player 2 (o, yellow) will begin!");
+            //board.draw()
+            while (true) {
+                //board.turnP2()
+                //board.draw()
+                //board.winCheck()
+                //board.turnP1()
+                //board.draw()
+                //board.winCheck()
+            }
+        }
+    }
 }
